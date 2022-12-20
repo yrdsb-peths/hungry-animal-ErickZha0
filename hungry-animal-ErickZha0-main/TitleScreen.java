a73ace8db0f8616992ef4accdde37d786ec2ada1
ErickZha0 <341137289@gapps.yrdsb.ca>
2022-12-20 14:43:23 -0500

    d

--- hungry-animal-ErickZha0-main/TitleScreen.java
+++ hungry-animal-ErickZha0-main/TitleScreen.java
@@ -8,51 +8,44 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class TitleScreen extends World
 {
-    Label titleLabel = new Label("The Elephant", 60);
+    Label titleLabel = new Label("Hungry Elephant", 60);
+
     /**
      * Constructor for objects of class TitleScreen.
      * 
      */
-    public TitleScreen()
-    {    
+    public TitleScreen() {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
 
-        addObject(titleLabel,getWidth()/2, 200);
+        addObject(titleLabel, getWidth()/2, getHeight()/2);
         prepare();
     }
-    // The main world act loop
-    public void act()
-    {
-        //Start the game if user presses the space bar
-        if(Greenfoot.isKeyDown("space"))
-        {
+
+    /*
+     * The main world act loop
+     */
+    public void act() {
+        // Starts game if user presses the space bar
+        if(Greenfoot.isKeyDown("space")) {
             MyWorld gameWorld = new MyWorld();
             Greenfoot.setWorld(gameWorld);
         }
     }
-
+    
     /**
      * Prepare the world for the start of the program.
      * That is: create the initial objects and add them to the world.
      */
     private void prepare()
     {
+        titleLabel.setLocation(300,60);
         Elephant elephant = new Elephant();
-        addObject(elephant,479,87);
-        elephant.setLocation(494,120);
-
-        Label label = new Label("Use \u2190 and \u2192 to Move", 40);
-        addObject(label,220,257);
-        label.setLocation(297,243);
-        label.setLocation(305,240);
-        
-        Label label2 = new Label("Press <Space> to Start", 40);
-        addObject(label2,249,331);
-        label2.setLocation(326,319);
-        label.setLocation(328,253);
-        label2.setLocation(295,326);
-        label.setLocation(278,261);
-
+        addObject(elephant,506,60);
+        Label label = new Label("Use \u2190 \u2192to Move", 40);
+        addObject(label,300,240);
+        elephant.setLocation(311,150);
+        Label label2 = new Label("Press <space> to Start", 30);
+        addObject(label2,297,321);
     }
 }
